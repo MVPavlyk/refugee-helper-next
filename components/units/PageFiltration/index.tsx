9deb03e550f8ec6de086d423a1c98c8b4c06cb12
Mykola Pavlyk <mykola.pavlyk@otakoyi.com>
2024-05-19 10:05:12 +0300

    feat: search

--- components/units/PageFiltration/index.tsx
+++ components/units/PageFiltration/index.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+'use client'
+
+import React, {useCallback, useEffect, useState} from 'react';
+import {useDebouncer} from '@/lib/hooks/useDebouncer';
+import {usePathname, useSearchParams, useRouter} from 'next/navigation';
 
 const PageFiltration = () => {
+    const searchParams = useSearchParams()
+
+    const paramsTitle = searchParams.get('Title') || ''
+
+    const [value, setValue] = useState(String(paramsTitle))
+
+    const debouncedValue = useDebouncer(value, 400)
+
+    const router = useRouter()
+    const pathname = usePathname()
+
+
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+            if(!value) return ''
+
+            const params = new URLSearchParams(searchParams.toString())
+            params.set(name, value)
+
+            return params.toString()
+        },
+        [searchParams]
+    )
+
+    useEffect(() => {
+        if (debouncedValue.length || paramsTitle.length) {
+            router.push(pathname + '?' + createQueryString('Title', debouncedValue))
+        }
+    }, [debouncedValue])
+
+
+    console.log(debouncedValue)
+
     return (
         <section className='w-full h-12 border-b border-gray-400 px-20 flex items-center justify-between'>
-            PageFiltration
+            <div/>
+            <input className='text-lg outline-none border-b-2 border-green-400' placeholder='Пошук' type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
         </section>
     );
 };
